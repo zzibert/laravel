13d0e8090e076c6d1b80f3ab27b3d81fd6ef942e
zan <zan_zibert@hotmail.com>
2019-01-31 18:26:42 +0100

    Step 5: Separate PHP Logic From Presentation

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-    <h1>
-        <?= "Hello, " . $_GET['name']; ?>
-    </h1>
-</body>
-</html>
+<?php
 
+$greeting = "hello, noobs!";
+
+require './index.view.php';
